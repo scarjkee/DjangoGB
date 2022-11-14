ac29ff2af710e6552ef71b62337162563004430c
scarjkee <frigtfullscar@mail.ru>
2022-11-14 22:21:44 +0500

    lesson_2 Static site with Django

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
-from mainapp import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="mainapp/")),
     path("mainapp/", include("mainapp.urls")),
+]
 ]
