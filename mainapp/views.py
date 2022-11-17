5388a39a5e340decaee909b4293c97aa7b821c2e
scarjkee <frigtfullscar@mail.ru>
2022-11-17 19:38:59 +0500

    lesson_3 Template's context

--- mainapp/views.py
+++ mainapp/views.py
@@ -3,9 +3,6 @@ from django.http import HttpResponse
 from django.views.generic import View
 from django.views.generic import TemplateView
 
-class HelloWorldView(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse("Hello world")
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
@@ -13,6 +10,16 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context[
+            "news_preview"
+        ] = "Предварительное описание, которое заинтересует каждого"
+        return context
 
 
 class CoursesPageView(TemplateView):
@@ -25,6 +32,10 @@ class ContactsPageView(TemplateView):
 
 class DocSitePageView(TemplateView):
     template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
 
 
 class LoginPageView(TemplateView):
